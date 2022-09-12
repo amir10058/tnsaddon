3af1d3f16aee3aa109b8fa485c191689472b78c5
amir <amir.bagheri@yahoo.com>
2022-09-13 01:14:54 +0430

    chore(bagheri, tmsaddon): first commit. base structure built. all unnecessary folders and files paths written in gitignore.

---
+++ src/main/java/de/ampada/tmsaddon/TmsaddonApplication.java
@@ -0,0 +1,13 @@
+package de.ampada.tmsaddon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsaddonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsaddonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/ampada/tmsaddon/TmsaddonApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ampada.tmsaddon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsaddonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
